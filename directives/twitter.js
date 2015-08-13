687a053e633dc7fe667e3a3315acb4aadf8fbfc1
Antti Stenvall <antti@stenvall.fi>
2015-08-13 21:32:46 +0300

    Added possibility to change url in twitter search on the fly

--- directives/twitter.js
+++ directives/twitter.js
@@ -2,35 +2,55 @@ angular.module('mcc').directive("mccTwitter", ['$http', 'mcc.toasterTranslate',
   function($http, toasterTranslate) {
     return {
       restrict: 'E',
-      scope: false, // could be true perhaps too, need to check
+      scope: {
+        url: '=url',
+        loadingMore: '=disableUrlChange'
+      }, // could be true perhaps too, need to check
       templateUrl: 'rest/mcc/templates/directives/twitter',
       link: function($scope, element, attrs) {
         $scope.isLoading = true;
         $scope.allFetched = false;
         $scope.data = [];
-        $scope.load = function() {
-          if ($scope.loadingMore || $scope.allFetched){
+
+        $scope.$watch('url', function() {          
+          if ($scope.url.length > 5) {
+            $scope.data = [];
+            $scope.allFetched = false;
+            $scope.load();            
+          }
+        });
+
+        $scope.load = function() {          
+          if ($scope.loadingMore || $scope.allFetched) {            
             return;
           }
           $scope.loadingMore = true;
           var maxid;
-          if ($scope.data.length == 0){
+          if ($scope.data.length == 0) {
             maxid = '';
           }
           else {
-            maxid = '/' + $scope.data[$scope.data.length-1].id;
+            maxid = '/' + $scope.data[$scope.data.length - 1].id;
           }
           $http({method: 'GET',
-            url: attrs.url + maxid}).
+            url: $scope.url + maxid}).
                   success(function(data, status, headers, config) {
-                    if (data.tweets.length == 0){
+                    if (data.tweets.length == 0) {
+                      $scope.loadingMore = false;
+                      $scope.allFetched = true;
+                      return;
+                    }
+                    if (data.tweets.length == 1 &&
+                            $scope.data.length > 0 &&
+                            ($scope.data[$scope.data.length - 1].id == data.tweets[0].id)) {
+                      $scope.loadingMore = false;
                       $scope.allFetched = true;
                       return;
                     }
                     // often twitter duplicates in homeline queries retweets
-                    if ($scope.data.length > 0){
-                      if ($scope.data[$scope.data.length-1].id == data.tweets[0].id){
-                        data.tweets.splice(0,1);
+                    if ($scope.data.length > 0) {
+                      if ($scope.data[$scope.data.length - 1].id == data.tweets[0].id) {
+                        data.tweets.splice(0, 1);
                       }
                     }
                     $scope.data = $scope.data.concat(data.tweets);
@@ -43,7 +63,7 @@ angular.module('mcc').directive("mccTwitter", ['$http', 'mcc.toasterTranslate',
                     $scope.loadingMore = false;
                   });
         };
-        
+
         $scope.load();
       }
     };
