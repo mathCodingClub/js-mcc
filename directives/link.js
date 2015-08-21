d2d56b7c5cb27827dc2d7ffa41f80aa6246af030
stenvala <antti@stenvall.fi>
2015-08-21 09:12:32 +0300

    Modified link

--- directives/link.js
+++ directives/link.js
@@ -1,25 +1,38 @@
 angular.module('mcc').directive('mccLink',
-        ['$location',function ($location) {
+        ['$location','$rootScope',function ($location,$rootScope) {
             return {
               restrict: 'AE',
               scope: {},
               link: function ($scope, element, attrs) {                
-                var clickfun = function(){               
-                  console.log('keijo');
-                  if ('url' in attrs){
-                    var url = attrs.url;
-                    if ('target' in attrs){
+                var extUrl = function(url){
+                  if ('target' in attrs){
                       window.open(url,attrs.target);
                     } 
                     else {
                       window.open(url);
                     }
+                };
+                var clickfun = function(){                                                   
+                  if ('url' in attrs){
+                    extUrl(attrs.url);                    
+                    return;
                   }
                   if ('route' in attrs){
                     $location.path(attrs.route);
+                    return;
+                  }                          
+                  if (attrs.mccLink.match('@://@') != ''){
+                    extUrl(attrs.mccLink);
+                  }
+                  else {
+                    $location.path(attrs.mccLink);                    
                   }                  
-                };                                
-                element.bind('tap click', clickfun);                               
+                };
+                var applyFun = function(){
+                  $rootScope.$apply(clickfun);
+                };
+                // add click
+                element.bind('tap click', applyFun);                               
                 $(element).addClass('hand');
                 if (!('noClass' in attrs)){
                   $(element).addClass('mcc-link');
