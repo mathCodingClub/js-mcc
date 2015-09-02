8df3286271f0724597f23775440de70e4f9da97b
stenvala <antti@stenvall.fi>
2015-09-02 08:36:12 +0300

    Added mccImgClick directive which opens image in new window when clicked, if attribte mcc-img-click is added to image

---
+++ directives/imgClick.js
@@ -0,0 +1,20 @@
+angular.module('mcc').directive('mccImgClick',
+        [function () {
+            return {
+              restrict: 'A',
+              scope: {},
+              link: function ($scope, element, attrs) {                
+                var clickFun = function(){
+                  var url = attrs.src;
+                  if ('target' in attrs){
+                      window.open(url,attrs.target);
+                    } 
+                    else {
+                      window.open(url);
+                    }
+                };                                                
+                element.bind('tap click', clickFun);                                               
+              }
+            };
+          }]
+        );
