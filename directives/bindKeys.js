99db7121501e8aefd65f96208adb7cea86d302a0
stenvala <antti@stenvall.fi>
2015-09-04 11:47:18 +0300

    added now and fixing ctrl-s for windows

--- directives/bindKeys.js
+++ directives/bindKeys.js
@@ -12,11 +12,10 @@ angular.module('mcc').directive('mccBindKeys', function () {
   return {restrict: 'A',
     link: function ($scope, element, attrs) {
       var bind = 'bindTo' in attrs ? attrs.bindTo : 'keydown';        
-      $(element)[bind](function (evt) {
-        console.log(evt);
+      $(element)[bind](function (evt) {        
         // ctrl-*
         var meta = attrs.mccBindKeys.match(/ctrl-[a-z]/);
-        if (meta && evt.metaKey) {                    
+        if (meta && (evt.metaKey || evt.ctrlKey)) {                    
           var ch = meta[0][5];                
           if (String.fromCharCode(evt.which).toLowerCase() === ch) {                        
             $scope[attrs['bindKeysCtrl' + ch.toUpperCase()]](evt);
